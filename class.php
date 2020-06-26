ab09694e18a938f9125bc6a2dfeacaf50812d7d9
CHAITANYA <Chaitanyas0101@gmail.com>
2020-06-26 15:20:56 +0530

    added test code

--- class.php
+++ class.php
@@ -1,3 +1,35 @@
 <?php
 echo "load_test";
+class FileUpload{
+	function Upload(){
+		  if(isset($_FILES['file'])){
+      $errors= array();
+      $file_name = $_FILES['file']['name'];
+      $file_size =$_FILES['file']['size'];
+      $file_tmp =$_FILES['file']['tmp_name'];
+      $file_type=$_FILES['file']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['file']['name'])));
+      $Upload_True = 1;
+      $Target_File= array("jpeg","jpg","png");
+      
+      if(in_array($file_ext,$Target_File)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+
+       if($file_size > 500000) {
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"images/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+
+   
+	}
+}
 ?>
--- index.php
+++ index.php
@@ -1,18 +1,41 @@
+
+<!--  <?php include 'class.php';?> -->
+<?php
+   if(isset($_FILES['file'])){
+      $errors= array();
+      $file_name = $_FILES['file']['name'];
+      $file_size =$_FILES['file']['size'];
+      $file_tmp =$_FILES['file']['tmp_name'];
+      $file_type=$_FILES['file']['type'];
+      $Upload_True = 1;
+      $Target_File= array("jpeg","jpg","png");
+      
+      if(in_array($file_ext,$Target_File)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+      
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"files/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css.css">
- <?php include 'class.php';?>
+
 </head>
-<body>
-<div class="container">
-    <form method="post" action="" enctype="multipart/form-data" id="FileUpload">
-        <div >
-            <input type="file" id="file" name="file" />
-            <input type="button" class="button" value="Upload" id="Upload_Button">
-        </div>
-    </form>
-</div>
-</script>
-</body>
+   <body>
+    <div class="container">
+      <form action="index.php" method="POST" enctype="multipart/form-data">
+         <input type="file" name="file" />
+         <input type="submit" class="button"/>
+      </form>
+      </div>
+   </body>
+</html> 
