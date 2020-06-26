473381c984852e1d540ef01e5dd0f06cbe088abd
CHAITANYA <Chaitanyas0101@gmail.com>
2020-06-26 14:23:22 +0530

    del few codes

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css.css">
+</head>
+<body>
+<div class="container">
+    <form method="post" action="" enctype="multipart/form-data" id="FileUpload">
+        <div >
+            <input type="file" id="file" name="file" />
+            <input type="button" class="button" value="Upload" id="Upload_Button">
+        </div>
+    </form>
+</div>
+</script>
+</body>
